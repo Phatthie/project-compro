0363455b9bb59ac1ac466686ec8cf8efa5dbb10a
Phatthie <younefayya@gmail.com>
2026-02-12 21:53:30 +0700

    seat

---
+++ 002.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int row=7,col=7;
+int a[10][10][7][7];
+
+void showSeatPlan(int movieIndex,int showtimeIndex){
+    for(int k=0;k<row;k++){
+        for(int r=0;r<col;r++){
+            if(a[movieIndex][showtimeIndex][k][r]==0) cout << "O ";
+            else cout << "X ";
+        }
+        cout << endl;
+    }
+}
+
+void resetSeat(int movieIndex,int showtimeIndex){
+    for(int k=0;k<row;k++){
+        for(int r=0;r<col;r++){
+            a[movieIndex][showtimeIndex][k][r] = 0;
+        }
+    }
+}
+
+void showBookingSum(int movieIndex,int showtimeIndex){
+    int available=0,booked=0;
+    for(int k=0;k<row;k++){
+        for(int r=0;r<col;r++){
+            if(a[movieIndex][showtimeIndex][k][r]==0) available += 1;
+            else booked += 1;
+         }
+    }
+    cout << "available seat: " << available << "\nBooked seat: " << booked;
+}
+
+int main(){
+    showSeatPlan(0,0);
+    showBookingSum(0,0);
+    resetSeat(0,0);
+}
